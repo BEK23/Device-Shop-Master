6c3ae677312861f211d10cde12e60db5a654c054
_BEK <62075760+BEK23@users.noreply.github.com>
2024-04-14 12:13:46 +0500

    add: debounce effect to search

--- src/pages/devices/components/DevicesToolbar.vue
+++ src/pages/devices/components/DevicesToolbar.vue
@@ -1,13 +1,29 @@
 <script lang="ts" setup>
 import { Search } from 'lucide-vue-next'
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 import { useCategoryStore } from '~/store/category.store'
 import { useDevicesStore } from '~/store/devices.store'
+
+const searchRef = ref('')
 
 const deviceStore = useDevicesStore()
 const categoryStore = useCategoryStore()
 
 const { category, search } = storeToRefs(deviceStore)
+
+const handleSearchInputChange = (function () {
+  let timer: ReturnType<typeof setTimeout> | null = null
+
+  return function (value: string) {
+    if (timer)
+      clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      search.value = value
+    }, 1000)
+  }
+})()
 </script>
 
 <template>
@@ -24,7 +40,13 @@ const { category, search } = storeToRefs(deviceStore)
         </el-select>
       </el-col>
       <el-col :span="10">
-        <el-input v-model="search" :prefix-icon="Search" placeholder="Search" clearable :debounce="1000" />
+        <el-input
+          v-model="searchRef"
+          :prefix-icon="Search"
+          placeholder="Search"
+          clearable
+          @input="handleSearchInputChange"
+        />
       </el-col>
     </el-row>
   </el-card>
