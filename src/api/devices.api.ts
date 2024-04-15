35f8a4e52c9e11ecb2d4f840c9e9d9d1a5c3d6d0
_BEK <62075760+BEK23@users.noreply.github.com>
2024-04-15 11:23:55 +0500

    useQuery -> watch

--- src/api/devices.api.ts
+++ src/api/devices.api.ts
@@ -1,33 +1,8 @@
 import type { AxiosResponse } from 'axios'
-import type { ComputedRef, Ref } from 'vue'
 import { API } from './api'
-import type { IDeviceRequest, IDeviceResponse, IDevicesListResponse } from '~/types/product.interface'
+import type { IDeviceRequest, IDeviceResponse } from '~/types/product.interface'
 
 // GET
-
-interface GetDevicesListParams {
-  category: Ref<number | undefined>
-  pageSize: Ref<number>
-  currentPage: Ref<number>
-  search: Ref<string>
-  sort?: ComputedRef<string>
-  order?: ComputedRef<string>
-}
-
-export function getDevicesList(
-  params: GetDevicesListParams,
-): Promise<AxiosResponse<IDevicesListResponse>> {
-  return API.get('/devices', {
-    params: {
-      page: params.currentPage.value,
-      limit: params.pageSize.value,
-      category: params.category.value,
-      ...(params.search.value && { search: params.search.value }),
-      ...(params.sort && { sort: params.sort.value }),
-      ...(params.order && { order: params.order.value }),
-    },
-  })
-}
 
 export function getDeviceByID(id: string): Promise<AxiosResponse<IDeviceResponse>> {
   return API.get(`/devices/${id}`)
--- src/components/layouts/DevicesLayout.vue
+++ src/components/layouts/DevicesLayout.vue
@@ -3,13 +3,11 @@ import { computed } from 'vue'
 import { Plus } from '@element-plus/icons-vue'
 
 import { useRoute } from 'vue-router'
-import { useQuery } from '@tanstack/vue-query'
 import { storeToRefs } from 'pinia'
 import DevicesTable from '~/components/devices/table/DevicesTable.vue'
 import DevicesToolbar from '~/components/devices/table/DevicesToolbar.vue'
 import { PATH } from '~/constants/path'
 import Breadcrumbs from '~/components/common/Breadcrumbs.vue'
-import { getDevicesList } from '~/api/devices.api'
 import { useDevicesStore } from '~/store/devices.store'
 
 const route = useRoute()
@@ -17,19 +15,7 @@ const route = useRoute()
 const deviceStore = useDevicesStore()
 
 const { devices, total, ...meta } = storeToRefs(deviceStore)
-const { setDevices, changePageSize, changeCurrentPage } = deviceStore
-
-const sort = computed(() => route.query.sort as string)
-const order = computed(() => route.query.order as string)
-
-useQuery({
-  queryKey: ['devices', { ...meta, sort, order }],
-  queryFn: () => getDevicesList({ ...meta, sort, order }),
-  select: (response) => {
-    setDevices(response.data)
-  },
-  refetchOnWindowFocus: false,
-})
+const { changePageSize, changeCurrentPage } = deviceStore
 
 const drawer = computed(() => route.name === 'devices.create' || route.name === 'devices.edit')
 
